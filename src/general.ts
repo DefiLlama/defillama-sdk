686d30403adba1894f3aca7008922a96deddb6fa
0xngmi <0xngmi@protonmail.com>
2021-03-18 23:31:33 +0100

    Add ability to use a different api key

--- src/general.ts
+++ src/general.ts
@@ -2,7 +2,7 @@ import { ethers, BigNumber } from "ethers";
 
 export const provider = new ethers.providers.AlchemyWebSocketProvider(
   "mainnet",
-  "50pap1Pw6npcNypxG15YCjj4W_K5kb3Z"
+  process.env.ALCHEMY_API ?? "50pap1Pw6npcNypxG15YCjj4W_K5kb3Z"
 );
 
 export const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
