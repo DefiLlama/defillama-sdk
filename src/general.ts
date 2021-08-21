79b3e9afde283b93518967ff993823cf8bcdb2a7
0xngmi <0xngmi@protonmail.com>
2021-08-21 16:08:59 +0100

    use failsafe providers

--- src/general.ts
+++ src/general.ts
@@ -1,112 +1,37 @@
 import { ethers, BigNumber } from "ethers";
 
+function createProvider(name: string, defaultRpc: string, chainId: number) {
+  return new ethers.providers.FallbackProvider(
+    (process.env[name.toUpperCase() + "_RPC"] ?? defaultRpc).split(',').map((url, i) => ({
+      provider: new ethers.providers.StaticJsonRpcProvider(
+        url,
+        {
+          name,
+          chainId,
+        }
+      ),
+      priority: i
+    })),
+    1
+  )
+}
+
 const providers = {
-  ethereum: new ethers.providers.StaticJsonRpcProvider(
-    process.env.ETHEREUM_RPC ??
-      "https://eth-mainnet.gateway.pokt.network/v1/5f3453978e354ab992c4da79",
-    {
-      name: "ethereum",
-      chainId: 1,
-    }
-  ),
-  bsc: new ethers.providers.StaticJsonRpcProvider(
-    process.env.BSC_RPC ?? "https://bsc-dataseed.binance.org/",
-    {
-      name: "bsc",
-      chainId: 56,
-    }
-  ),
-  polygon: new ethers.providers.StaticJsonRpcProvider(
-    process.env.POLYGON_RPC ?? "https://rpc-mainnet.maticvigil.com/",
-    {
-      name: "polygon",
-      chainId: 137,
-    }
-  ),
-  heco: new ethers.providers.StaticJsonRpcProvider(
-    process.env.HECO_RPC ?? "https://http-mainnet.hecochain.com",
-    {
-      name: "heco",
-      chainId: 128,
-    }
-  ),
-  fantom: new ethers.providers.StaticJsonRpcProvider(
-    process.env.FANTOM_RPC ?? "https://rpcapi.fantom.network",
-    {
-      name: "fantom",
-      chainId: 250,
-    }
-  ),
-  rsk: new ethers.providers.StaticJsonRpcProvider(
-    process.env.RSK_RPC ?? "https://public-node.rsk.co",
-    {
-      name: "rsk",
-      chainId: 30,
-    }
-  ),
-  tomochain: new ethers.providers.StaticJsonRpcProvider(
-    process.env.TOMOCHAIN_RPC ?? "https://rpc.tomochain.com",
-    {
-      name: "tomochain",
-      chainId: 88,
-    }
-  ),
-  xdai: new ethers.providers.StaticJsonRpcProvider(
-    process.env.XDAI_RPC ?? "https://xdai.poanetwork.dev",
-    {
-      name: "xdai",
-      chainId: 100,
-    }
-  ),
-  avax: new ethers.providers.StaticJsonRpcProvider(
-    process.env.AVAX_RPC ?? "https://api.avax.network/ext/bc/C/rpc",
-    {
-      name: "avax",
-      chainId: 43114,
-    }
-  ),
-  wan: new ethers.providers.StaticJsonRpcProvider(
-    process.env.WAN_RPC ?? "https://gwan-ssl.wandevs.org:56891",
-    {
-      name: "wan",
-      chainId: 888,
-    }
-  ),
-  harmony: new ethers.providers.StaticJsonRpcProvider(
-    process.env.HARMONY_RPC ?? "https://api.s0.t.hmny.io",
-    {
-      name: "harmony",
-      chainId: 1666600000,
-    }
-  ),
-  thundercore: new ethers.providers.StaticJsonRpcProvider(
-    process.env.THUNDERCORE_RPC ?? "https://mainnet-rpc.thundercore.com",
-    {
-      name: "thundercore",
-      chainId: 108,
-    }
-  ),
-  okexchain: new ethers.providers.StaticJsonRpcProvider(
-    process.env.OKEXCHAIN_RPC ?? "https://exchainrpc.okex.org",
-    {
-      name: "okexchain",
-      chainId: 66,
-    }
-  ),
-  optimism: new ethers.providers.StaticJsonRpcProvider(
-    process.env.OPTIMISM_RPC ?? "https://mainnet.optimism.io/",
-    {
-      name: "optimism",
-      chainId: 10,
-    }
-  ),
-  arbitrum: new ethers.providers.StaticJsonRpcProvider(
-    process.env.ARBITRUM_RPC ?? "https://arb1.arbitrum.io/rpc",
-    {
-      name: "arbitrum",
-      chainId: 42161,
-    }
-  ),
+  ethereum: createProvider("ethereum", "https://eth-mainnet.gateway.pokt.network/v1/5f3453978e354ab992c4da79", 1),
+  bsc: createProvider("bsc","https://bsc-dataseed.binance.org/",56),
+  polygon: createProvider( "polygon","https://rpc-mainnet.maticvigil.com/",137),
+  heco: createProvider("heco", "https://http-mainnet.hecochain.com", 128),
+  fantom: createProvider("fantom", "https://rpcapi.fantom.network", 250),
+  rsk: createProvider("rsk", "https://public-node.rsk.co", 30),
+  tomochain: createProvider("tomochain", "https://rpc.tomochain.com", 88),
+  xdai: createProvider("xdai", "https://xdai.poanetwork.dev", 100),
+  avax: createProvider("avax","https://api.avax.network/ext/bc/C/rpc",43114),
+  wan: createProvider("wan", "https://gwan-ssl.wandevs.org:56891", 888),
+  harmony: createProvider("harmony", "https://api.s0.t.hmny.io", 1666600000),
+  thundercore: createProvider("thundercore", "https://mainnet-rpc.thundercore.com", 108),
+  okexchain: createProvider("okexchain", "https://exchainrpc.okex.org", 66),
+  optimism: createProvider( "optimism", "https://mainnet.optimism.io/",10),
+  arbitrum: createProvider("arbitrum", "https://arb1.arbitrum.io/rpc", 42161),
 } as {
   [chain: string]: ethers.providers.BaseProvider;
 };
