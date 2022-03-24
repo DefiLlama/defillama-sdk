67abdadff5f1bd5e4ccf1e5797df76172c019441
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-03-24 13:19:15 +0100

    Reduced wait time, made it configurable

--- src/abi/rpcCall.ts
+++ src/abi/rpcCall.ts
@@ -1,18 +1,38 @@
 import { Deferrable } from "@ethersproject/properties"
 import { BaseProvider, BlockTag, TransactionRequest } from "@ethersproject/providers"
 
-const maxParallelCalls = 100
+const DEBUG_MODE_ENABLED = !!process.env.LLAMA_DEBUG_MODE
+const maxParallelCalls = !!process.env.LLAMA_SDK_MAX_PARALLEL ? +process.env.LLAMA_SDK_MAX_PARALLEL : 100
+const waitTime = !!process.env.LLAMA_SDK_CALL_WAIT_TIME ? +process.env.LLAMA_SDK_CALL_WAIT_TIME : 50
+
 let activeWorkers = 0
+let queueCount = 0
+let requestCount = 0
 
 export async function call(provider: BaseProvider, data: Deferrable<TransactionRequest>, block: BlockTag) {
-  while (activeWorkers > maxParallelCalls)
+  const currentId = requestCount++
+  let addedToQueue = false
+
+  while (activeWorkers > maxParallelCalls) {
+    if (DEBUG_MODE_ENABLED && !addedToQueue) {
+      addedToQueue = true
+      queueCount++
+    }
     await wait()
+  }
+
   activeWorkers++
+
+  if (DEBUG_MODE_ENABLED) {
+    if (addedToQueue) queueCount--
+    if (queueCount && currentId % 50 === 0) console.log(`#: ${currentId} queue: ${queueCount} active requests: ${activeWorkers}`)
+  }
+
   const response = await provider.call(data, block)
   activeWorkers--
   return response
 }
 
-async function wait(time = 500) {
+async function wait(time = waitTime) {
   return new Promise((resolve) => setTimeout(resolve, time))
 }
