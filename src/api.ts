2df00b9d324d73d36dfce2e6824f7dcdad0ec1aa
Musket <musket.gun@position.exchange>
2022-08-23 15:26:46 +0700

    export rpc in api

--- src/api.ts
+++ src/api.ts
@@ -3,6 +3,7 @@ export * as eth from "./eth";
 export * as erc20 from "./erc20";
 export * as cdp from "./cdp";
 export * as abi from "./abi";
+export * as rpc from "./rpc";
 import { setProvider } from "./general";
 const config = {
   setProvider,
