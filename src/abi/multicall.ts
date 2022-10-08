39dc5681afb5d1d02b331ca83a2db704cfbc16e6
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-10-08 23:35:43 +0200

    add dfk multicall

--- src/abi/multicall.ts
+++ src/abi/multicall.ts
@@ -272,6 +272,8 @@ function multicallAddress(chainId: number) {
       return "0x28d2ebdb36369db1c51355cdc0898754d1a1c3c5";
     case 1234:
       return "0x176CcFFbAB792Aaa0da7C430FE20a7106d969f66"
+    case 53935: // dfk
+      return "0x5b24224dC16508DAD755756639E420817DD4c99E"
     default:
       return null;
   }
