5f4110c81a727c6314b30eba09c6f692067ff593
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-03-03 07:00:57 +0100

    minor fix

--- patchAndPublish.sh
+++ patchAndPublish.sh
@@ -6,10 +6,12 @@ if [[ $? -ne 0 ]] ; then
   echo "Unit test failed, fix it before publishing new version"
   exit 1
 fi
+git checkout master -- src/providers.json # reset file else version patching wont work
 npm version patch
 git push
 rm -rf build
 rm LICENSE
+ts-node src/util/updateProviderList.ts
 npm publish
 git checkout master -- LICENSE
 git checkout master -- src/providers.json
