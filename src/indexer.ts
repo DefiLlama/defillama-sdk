388ebbadd4ee5b7e7483958bbe359b82d47344cb
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-09-18 09:17:16 +0200

    WIP

--- src/indexer.ts
+++ src/indexer.ts
@@ -9,23 +9,50 @@ const indexerURL = getEnvValue('LLAMA_INDEXER_ENDPOINT')
 
 const indexerChainIdChainMapping: any = {
   1: 'ethereum',
-  56: 'bsc',
-  137: 'polygon',
-  250: 'fantom',
-  43114: 'avalanche',
   10: 'optimism',
-  42161: 'arbitrum',
+  56: 'bsc',
   100: 'xdai',
+  137: 'polygon',
   204: 'op_bnb',
+  250: 'fantom',
   324: 'era',
-  8453: 'base',
-  42170: 'arbitrum_nova',
   1101: 'polygon_zkevm',
+  8453: 'base',
+  34443: 'mode',
+  42170: 'arbitrum_nova',
+  42161: 'arbitrum',
+  43114: 'avalanche',
   59144: 'linea',
+  81457: 'blast',
   534352: 'scroll',
 }
 
-export async function getTokens(address: string, { onlyWhitelisted = true, skipCacheRead = false } = {}) {
+export const supportedChainSet = new Set(Object.values(indexerChainIdChainMapping))
+const chainToIDMapping: any = {}
+Object.entries(indexerChainIdChainMapping).forEach(([id, chain]: any) => {
+  chainToIDMapping[chain] = id
+})
+
+
+enum TokenTypes {
+  ERC20 = 'erc20',
+  ERC721 = 'erc721',
+}
+
+type Cache = {
+  timestamp: number
+  tokens: {
+    [chain: string]: string[]
+  }
+}
+
+export async function getTokens(address: string, { onlyWhitelisted = true, skipCacheRead = false, skipCache = false, chain, tokenType }: {
+  onlyWhitelisted?: boolean,
+  skipCacheRead?: boolean,
+  skipCache?: boolean,
+  chain?: string,
+  tokenType?: TokenTypes,
+} = {}) {
 
   if (!indexerURL) throw new Error('Llama Indexer URL not set')
 
@@ -34,14 +61,21 @@ export async function getTokens(address: string, { onlyWhitelisted = true, skipC
   const file = `${project}/${key}`
   const timeNow = Math.floor(Date.now() / 1e3)
   const THREE_DAYS = 3 * 24 * 3600
-  const cache = (await readCache(file)) ?? {}
-  if (!skipCacheRead && cache.timestamp && (timeNow - cache.timestamp) < THREE_DAYS)
-    return cache.tokens
-
+  let cache = {} as Cache
+  if (!skipCacheRead && !skipCache) {
+    cache = (await readCache(file)) ?? {}
+    if (cache.timestamp && (timeNow - cache.timestamp) < THREE_DAYS)
+      return cache.tokens
+  }
   debugLog('Pulling tokens for ' + address)
 
   const tokens = cache.tokens ?? {}
-  const { data: { balances } } = await axios.get(`${indexerURL}/balances?address=${address}`)
+  const { data: { balances } } = await axios.get(`${indexerURL}/balances?address=${address}`, {
+    params: {
+      chain: chain,
+      type: tokenType,
+    }
+  })
   balances.filter((b: any) => +b.total_amount > 0).forEach((b: any) => {
     const chain = indexerChainIdChainMapping[b.chain]
     if (!chain) {
@@ -56,6 +90,57 @@ export async function getTokens(address: string, { onlyWhitelisted = true, skipC
     tokens[chain] = getUniqueAddresses(values)
   })
 
-  await writeCache(file, tokenCache)
+  if (!skipCache)
+    await writeCache(file, tokenCache)
   return tokens
+}
+
+export async function getLogs({ address, chain, topic0, fromBlock, toBlock, all = true, limit = 1000, offset = 0 }: {
+  address: string,
+  chain: string,
+  topic0: string,
+  fromBlock: number,
+  toBlock: number,
+  limit?: number,
+  offset?: number,
+  all?: boolean,
+}) {
+  if (!indexerURL) throw new Error('Llama Indexer URL not set')
+  const chainId = chainToIDMapping[chain]
+  if (!chainId) throw new Error('Chain not supported')
+  let hasMore = true
+  const logs: any[] = []
+  do {
+    const params = {
+      address,
+      chain: chainId,
+      topic: topic0,
+      from_block: fromBlock,
+      to_block: toBlock,
+      limit,
+      offset,
+    }
+    const { data: { logs } } = await axios.get(`${indexerURL}/logs`, { params })
+    offset += limit
+  } while (all && hasMore)
+  
+
+  const project = 'llama-indexer-cache'
+  const key = address
+  const file = `${project}/${key}`
+  const timeNow = Math.floor(Date.now() / 1e3)
+  const THREE_DAYS = 3 * 24 * 3600
+  let cache = {} as any
+  if (!skipCacheRead && !skipCache) {
+    cache = (await readCache(file)) ?? {}
+    if (cache.timestamp && (timeNow - cache.timestamp) < THREE_DAYS)
+      return cache.logs
+  }
+  debugLog('Pulling logs for ' + address)
+
+  const { data: { logs } } = await axios.get(`${indexerURL}/logs?address=${address}`)
+  const logCache = { timestamp: timeNow, logs }
+  if (!skipCache)
+    await writeCache(file, logCache)
+  return logs
 }
