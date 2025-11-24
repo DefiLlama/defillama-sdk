cbd9b2c2f5e1e528969a43fbf3de08c469a0845c
0xpeluche <110820448+0xpeluche@users.noreply.github.com>
2025-11-24 10:15:57 +0100

    add indexer timeout (#192)
    * add indexer timeout
    
    * default value

--- src/util/indexer.ts
+++ src/util/indexer.ts
@@ -22,6 +22,7 @@ const LLAMA_INDEXER_API_KEY = getEnvValue("LLAMA_INDEXER_API_KEY");
 const LLAMA_INDEXER_V2_ENDPOINT = getEnvValue("LLAMA_INDEXER_V2_ENDPOINT");
 const LLAMA_INDEXER_V2_API_KEY = getEnvValue("LLAMA_INDEXER_V2_API_KEY");
 const addressChunkSize = +getEnvValue("LLAMA_INDEXER_ADDRESS_CHUNK_SIZE")! || 100;
+const INDEXER_REQUEST_TIMEOUT_MS = +getEnvValue("LLAMA_INDEXER_TIMEOUT_MS")!;
 
 // v1 chains (still used for balances endpoint)
 const indexerChainIdChainMapping: { [key: number]: string } = {
@@ -82,14 +83,14 @@ const axiosInstances = {
   v1: axios.create({
     headers: { "x-api-key": indexerConfigs.v1.apiKey },
     baseURL: indexerConfigs.v1.endpoint,
-    timeout: 180_000
+    timeout: INDEXER_REQUEST_TIMEOUT_MS,
   }),
   v2: axios.create({
     headers: { "x-api-key": indexerConfigs.v2.apiKey },
     baseURL: indexerConfigs.v2.endpoint,
     httpAgent,
     httpsAgent,
-    timeout: 300_000
+    timeout: INDEXER_REQUEST_TIMEOUT_MS,
   }),
 };
 
