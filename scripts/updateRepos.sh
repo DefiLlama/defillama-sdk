575f35e3d9b41b87278d9992f39f7406d7553422
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-05-12 13:15:20 +0200

    pin sdk version the dependencies

--- scripts/updateRepos.sh
+++ scripts/updateRepos.sh
@@ -3,91 +3,58 @@ ROOT_DIR=$SCRIPT_DIR/..
 
 cd $ROOT_DIR
 
+SDK_VERSION=$(node -p "require('./package.json').version")
+echo "Pinning @defillama/sdk to $SDK_VERSION"
+
 mkdir -p depRepos
 cd depRepos
 
-echo "Update tvl repo"
+# Pin @defillama/sdk to $SDK_VERSION in package.json (any range/spec).
+pin_sdk_version() {
+  node -e "
+    const fs = require('fs');
+    const p = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+    let changed = false;
+    for (const k of ['dependencies', 'devDependencies', 'peerDependencies', 'optionalDependencies']) {
+      if (p[k] && p[k]['@defillama/sdk']) {
+        p[k]['@defillama/sdk'] = '$SDK_VERSION';
+        changed = true;
+      }
+    }
+    if (changed) fs.writeFileSync('package.json', JSON.stringify(p, null, 2) + '\n');
+  "
+}
 
-[ ! -d "DefiLlama-Adapters" ] && git clone git@github.com:DefiLlama/DefiLlama-Adapters.git
+# update_repo <repo-name> <git-url> <branch> [subdir1 subdir2 ...]
+update_repo() {
+  local name=$1 url=$2 branch=$3
+  shift 3
+  local subdirs=("$@")
+  [ ${#subdirs[@]} -eq 0 ] && subdirs=(".")
 
-cd DefiLlama-Adapters
-git stash
-git checkout main
-git pull
-pnpm i
-pnpm update @defillama/sdk
-git add package.json pnpm-lock.yaml
-git commit -m "update @defillama/sdk version"
-git push
-cd ..
+  echo "Update $name"
+  [ ! -d "$name" ] && git clone "$url"
 
+  pushd "$name" > /dev/null
+  git stash
+  git checkout "$branch"
+  git pull
 
-echo "Update dimension repo"
+  for sub in "${subdirs[@]}"; do
+    pushd "$sub" > /dev/null
+    pin_sdk_version
+    pnpm i
+    git add package.json pnpm-lock.yaml
+    popd > /dev/null
+  done
 
-[ ! -d "dimension-adapters" ] && git clone git@github.com:DefiLlama/dimension-adapters.git
+  git commit -m "update @defillama/sdk version"
+  git push
+  popd > /dev/null
+}
 
-cd dimension-adapters
-git stash
-git checkout master
-git pull
-pnpm i
-pnpm update @defillama/sdk
-git add package.json pnpm-lock.yaml
-git commit -m "update @defillama/sdk version"
-git push
-cd ..
-
-
-echo "Update server repo"
-
-[ ! -d "defillama-server" ] && git clone git@github.com:DefiLlama/defillama-server.git
-
-cd defillama-server
-git stash
-git checkout master
-git pull
-cd defi
-pnpm i
-pnpm update @defillama/sdk
-git add package.json pnpm-lock.yaml
-
-cd ../coins
-pnpm i
-pnpm update @defillama/sdk
-git add package.json pnpm-lock.yaml
-
-git commit -m "update @defillama/sdk version"
-git push
-cd ../..
-
-
-echo "Update peggedassets repo"
-
-[ ! -d "peggedassets-server" ] && git clone git@github.com:DefiLlama/peggedassets-server.git
-
-cd peggedassets-server
-git stash
-git checkout master
-git pull
-pnpm i
-pnpm update @defillama/sdk
-git add package.json pnpm-lock.yaml
-git commit -m "update @defillama/sdk version"
-git push
-cd ..
-
-
-echo "Update emissions repo"
-
-[ ! -d "emissions-adapters" ] && git clone git@github.com:DefiLlama/emissions-adapters.git
-
-cd emissions-adapters
-git stash
-git checkout master
-git pull
-pnpm i
-pnpm update @defillama/sdk
-git add package.json pnpm-lock.yaml
-git commit -m "update @defillama/sdk version"
-git push
-cd ..
+update_repo DefiLlama-Adapters  git@github.com:DefiLlama/DefiLlama-Adapters.git  main
+update_repo dimension-adapters  git@github.com:DefiLlama/dimension-adapters.git  master
+update_repo defillama-server    git@github.com:DefiLlama/defillama-server.git    master  defi coins
+update_repo peggedassets-server git@github.com:DefiLlama/peggedassets-server.git master
+update_repo emissions-adapters  git@github.com:DefiLlama/emissions-adapters.git  master
