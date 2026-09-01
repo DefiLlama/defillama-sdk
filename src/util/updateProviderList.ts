8a9f5a024ac8b75eb0024a16464c2772b36f143c
wayne <waynebruce0x@gmail.com>
2026-09-01 13:49:30 +0100

    patch

--- src/util/updateProviderList.ts
+++ src/util/updateProviderList.ts
@@ -196,7 +196,7 @@ function filterRPCs(rpc: string[]): string[] {
     if (!i.startsWith('http')) return false // remove demo rpc
     if (i.includes('$')) return false // remove anything where api key is injected
     // reject websocket, http, testnet, devnet, and anything with '='
-    if (/(wss\:|ws\:|http\:|test|devnet|terminet\.io|huobichain\.com|getblock\.io|bitstack\.com|nodereal.io\/v1|pokt\.network|ankr\.com|histori\.xyz|chainstacklabs\.com|owlracle\.info|blastapi\.io|\=)/.test(i)) return false // remove anything with blacklisted words
+    if (/(wss\:|ws\:|http\:|test|devnet|terminet\.io|huobichain\.com|getblock\.io|bitstack\.com|nodereal.io\/v1|pokt\.network|ankr\.com|lb\.routeme\.sh|histori\.xyz|chainstacklabs\.com|owlracle\.info|blastapi\.io|\=)/.test(i)) return false // remove anything with blacklisted words
     return true
   }).map((i: string) => {
     if (i.endsWith('/')) return i.slice(0, -1)
